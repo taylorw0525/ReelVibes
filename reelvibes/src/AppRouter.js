46962e0c0a09d9c147ccac8b710fb7468e7a486e
taylorw0525 <taylorw0525@gmail.com>
2025-04-30 12:48:41 -0400

    forms are created

--- reelvibes/src/AppRouter.js
+++ reelvibes/src/AppRouter.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import App from "./App"; // Default app component
-import Navbar from "./common/Navbar";
-import MoodPage from "./MoodSelection/MoodPage"; // Mood page component
-import MoviePage from "./MoodSelection/MoviePage";
+import App from "./App"; // Default home component
+import Navbar from "./common/Navbar"; // Navigation bar
+import MoodPage from "./MoodSelection/MoodPage"; // Mood selection
+import MoviePage from "./MoodSelection/MoviePage"; // Movie details
+import Login from "./Forms/Login"; 
+import Signup from "./Forms/Signup";
 
 const AppRouter = () => {
   return (
     <Router>
-        <Navbar /> {/* Navbar component for navigation */}
+      <Navbar />
       <Routes>
-        {/* Default route ("/") renders the App component */}
         <Route path="/" element={<App />} />
-
-        {/* "/moodpage" renders the MoodPage component */}
+        <Route path="/login" element={<Login />} />
         <Route path="/moodpage" element={<MoodPage />} />
-
-        {/* "/moviepage" renders the MoviePage component*/}
         <Route path="/moviepage" element={<MoviePage />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
