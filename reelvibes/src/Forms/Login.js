6fffc4ba6fe3ecbd375d6b088d0cf9ef54e64339
Suru <172854381+Surachhya@users.noreply.github.com>
2025-05-05 12:33:41 -0400

    more setups

--- reelvibes/src/Forms/Login.js
+++ reelvibes/src/Forms/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     e.preventDefault();
   
     try {
-      const res = await fetch("http://localhost:5000/api/users/login", {
+      const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/users/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
